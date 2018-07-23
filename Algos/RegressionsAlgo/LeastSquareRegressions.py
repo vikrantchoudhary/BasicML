43cbe901172b07bc017a1f1d5e7db144d944e977
vikrantchoudhary <vikrantchoudhary@gmail.com>
2018-07-23 14:14:09 +0530

    other demo of Linear regression

---
+++ Algos/RegressionsAlgo/LeastSquareRegressions.py
@@ -0,0 +1,4 @@
+# what exactly is LeastSquareRegression..
+# You can draw a line, and then for each of the data points,
+# measure the vertical distance between the point and the line, and add these up;
+# the fitted line would be the one where this sum of distances is as small as possible
---
+++ Algos/RegressionsAlgo/LogisticRegression.py
@@ -0,0 +1,3 @@
+#Logistic regression is a powerful statistical way of modeling a binomial outcome with one or more explanatory variables.
+# It measures the relationship between the categorical dependent variable and one or more independent variables by
+# estimating probabilities using a logistic function, which is the cumulative logistic distribution.
