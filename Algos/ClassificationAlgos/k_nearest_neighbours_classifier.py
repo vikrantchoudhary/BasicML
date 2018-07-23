084c513fd7ef297b94476f0afdcef083d100c0b9
vikrantchoudhary <vikrantchoudhary@gmail.com>
2018-07-23 15:44:55 +0530

    classification algos.

---
+++ Algos/ClassificationAlgos/k_nearest_neighbours_classifier.py
@@ -0,0 +1,3 @@
+# a classification algorithm for a point to  find best suited  grp.
+# calculates euclidean distance from the given point to all point
+# and then smallest distance will be best suited classification for the given point
---
+++ Algos/ClassificationAlgos/support_vector_machine_classifier.py
@@ -0,0 +1,1 @@
+# n-dimensional points can be classified by taking n-1 and 1 point so that it will becomes 2-D and then find best fitting hyperplane.
